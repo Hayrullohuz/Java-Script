8e33f9909641adfa119d86c891857c56320771d9
Hayrullohuz <hayrulloh1706@gmail.com>
2024-01-31 15:46:49 +0600

    Start commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// "use strict";
+// const person = {
+//   name: 'khairulloh'
+// };
+// person.name = 'ali';
+// console.log(person)
+
+
+// let incr = 10;
+// let decr = 20;
+// incr++;
+// decr--;
+// console.log(incr)
+// console.log(decr)
+
+
+// let incr = 20;
+// let decr = 20;
+// console.log (incr++)
+// console.log (decr--)
+// console.log (incr)
+// console.log (decr)
+
+
+// console.log (10 % 5)
+
+
+// console.log (5*2 == 10)
+
+
+// console.log (2*2 == '4')
+
+const age = true;
+const long = false;
+console.log (age && long)
